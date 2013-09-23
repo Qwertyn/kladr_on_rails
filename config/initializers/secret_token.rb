1c24b5050a2eccd8bc47cb687a3f9d87602a2aaa
Aleksandr Salkov <asalkov@at-consulting.ru>
2013-09-23 19:09:00 +0400

    Создано чистое приложение на rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KladrOnRails::Application.config.secret_key_base = 'c87cd541fb40a3aec082f9ace51b64fd3e98da8c445877526b375506cc48558c8bd256397c15c53dcda2212b0239ee2659290ede64b28e97a0656084f13af3a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KladrOnRails::Application.config.session_store :cookie_store, key: '_kladr_on_rails_session'
